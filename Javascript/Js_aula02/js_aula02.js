338968e7d85d7e30f727ed3f3640c6969910a63c
GVScode <guilhermmesantos98@gmail.com>
2022-10-10 21:05:15 -0300

    subida da atividade referente a aula 02.js

---
+++ Javascript/Js_aula02/js_aula02.js
@@ -0,0 +1,9 @@
+var nota1 = 10;
+var nota2 = 10;
+var nota3 = 10;
+var nota4 = 10;
+
+var Media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+ console.log("A média do aluno com as 4 notas é :" + Media); 
