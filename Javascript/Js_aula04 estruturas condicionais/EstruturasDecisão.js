ee224d88f54f4133c623f0db08bbb443b7cc5b97
GVScode <guilhermmesantos98@gmail.com>
2022-10-11 21:42:15 -0300

    subindo arquivos referente a aula 04 de js

---
+++ Javascript/Js_aula04 estruturas condicionais/EstruturasDecisão.js
@@ -0,0 +1,14 @@
+
+const produto01 = 4;
+const produto02 = 11;
+const produto03 = 4;
+
+if (produto01 > produto02 || g > 10) {
+console.log(`Condição é ${f > 5 || g > 10}`);
+console.log('f é maior do que 5 ou g é maior do 10');
+} else console.log('f não é maior do que 5 e g não é maior do 10');
+if (f > 5 && g > 10) console.log('f é maior do que 5 e g é maior do 10');
+else {
+console.log(`Condição é ${f > 5 && g > 10}`);
+console.log('f não é maior do que 5 ou g não é maior do 10');
+}
