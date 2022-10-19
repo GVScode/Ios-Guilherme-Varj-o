e4a8ea5ddd4e76c692e5dcbab1627d7fdf6de021
GVScode <guilhermmesantos98@gmail.com>
2022-10-18 21:48:26 -0300

    subindo arquivo referente a aula 07 array de objetos

---
+++ Javascript/JS_aula07 arrays de objetos/Atv_Arrays/Atv_araysDeObjetos.js
@@ -0,0 +1,47 @@
+// Array de objetos
+const Alunos = [
+    {
+        id: 1,
+        texto: 'Nota',
+        isCompleted: true,
+    },
+    {
+        id: 2,
+        texto: 'Endereço',
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        texto: 'aluno1',
+        isCompleted: false,
+    },
+    {
+        id: 4,
+        texto: 'Levar o lixo para fora',
+        isCompleted: true,
+    },
+    {
+        id: 5,
+        texto: 'Encontrar com o chefe',
+        isCompleted: true,
+    },
+    {
+        id: 6,
+        texto: 'Consulta no dentista',
+        isCompleted: false,
+    },
+    {
+        id: 7,
+        texto: 'Levar o lixo para fora',
+        isCompleted: true,
+    },
+    {
+        id: 8,
+        texto: 'Encontrar com o chefe',
+        isCompleted: true,
+    },
+   
+];
+console.log(id);
+console.log(id[1].texto);
+console.log(id[2].id, alunos[2].texto, alunos[2].isCompleted);
