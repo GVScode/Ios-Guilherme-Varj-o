9499031c42cbf00d55f849a3e580e6ed66c8e1be
GVScode <guilhermmesantos98@gmail.com>
2022-10-25 21:01:21 -0300

    arquivos referente a aula 04 Estruturas de decisão

--- Javascript/Js_aula04 estruturas condicionais/EstruturasDecisão.js
+++ Javascript/Js_aula04 estruturas condicionais/EstruturasDecisão.js
@@ -1,14 +1,68 @@
 
-const produto01 = 4;
-const produto02 = 11;
-const produto03 = 4;
+/* 1) Receber o preço de três produtos e informar qual
+produto possui o menor valor. */
 
-if (produto01 > produto02 || g > 10) {
-console.log(`Condição é ${f > 5 || g > 10}`);
-console.log('f é maior do que 5 ou g é maior do 10');
-} else console.log('f não é maior do que 5 e g não é maior do 10');
-if (f > 5 && g > 10) console.log('f é maior do que 5 e g é maior do 10');
-else {
-console.log(`Condição é ${f > 5 && g > 10}`);
-console.log('f não é maior do que 5 ou g não é maior do 10');
+let tênis = 300
+let camiseta =  100
+let boné = 50
+
+if(tênis < camiseta && tênis < boné){
+    console.log(`esse é o menor preço ${tênis}`)
+}else if(camiseta < tênis && camiseta < boné){
+    console.log(`esse é o menor preço ${camiseta}`)
+}else{
+    console.log(`o produto mais barato tem o seguinte valor: R$${boné.toFixed(2)}`)
+}
+
+//2)Receber três números e mostrar em ordem crescente
+
+let num1 = 1
+let num2 = 2
+let num3 = 3
+let maior = 4
+let meio = 5
+let menor = 6
+
+if(num1 > num2 && num1 > num3){
+    maior = num1
+
+    if(num2 > num3){
+        meio = num2
+        menor = num3
+    }else{
+        menor = num2
+        meio = num3
+    }
+    
+}if(num1 < num2 && num1 < num3){
+    menor = num1
+        if(num2 > num3){
+            maior = num2
+            meio = num3
+        }else{
+            maior = num3
+            meio = num2
+        }
+}else{
+    meio = num1
+        if(num2 > num3){
+            maior = num2
+            menor = num3
+        }else{
+            maior = num3
+            menor = num2
+        }
+}
+
+console.log(`${menor} ${meio} ${maior}`)
+
+/*3)Receber um número correspondente a um determinado
+ano e em seguida informar se este ano é bissexto*/
+
+let ano = 2024 
+
+if(ano % 4 == 0 || ano % 400 == 0){
+    console.log(`${ano} é um ano bissexto`)
+}else{
+    console.log(`${ano} não é um ano é bissexto!!!`)
 }
