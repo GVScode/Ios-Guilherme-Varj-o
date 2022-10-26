bc924f70df93be924317a05d3f1ed24d951afce3
GVScode <guilhermmesantos98@gmail.com>
2022-10-25 21:24:24 -0300

    aula 07 arrays

--- Javascript/Js_aula06 Metodos de arrays/Atv_arrays/Atv_arrays.js
+++ Javascript/Js_aula06 Metodos de arrays/Atv_arrays/Atv_arrays.js
@@ -1,20 +1,49 @@
-/*Criando array unidimensional*/
-/* todo array*/
-/*var array = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "Js", 1995, "React", 2013, "Java", 1995]
-console.log (array);*/
-
-/*tamanho do array e imprimindo JS*/
-/*console.clear();
-let array = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "Js", 1995, "React", 2013, "Java", 1995];
-console.log(array.length);
-console.log(array[6])*/
+// Exercicio 2
+var array1 = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "JS", 1995, "React", 2013, "Java", 1995]
+console.log(array1);
 
 
+console.log(array1.length)
 
-// alterando para string e mostrando o tipo
-//alterando o tipo *** para /
-/*console.clear();
-let teste1 = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "Js", 1995, "React", 2013, "Java", 1995];
-let teste2 = teste1.join(' / ');
-console.log(teste2);
-console.log(typeof teste2);*/
+
+console.log(array1[6]);
+
+
+var x = array1.toString();
+console.log(typeof x)
+console.log(typeof array1)
+console.log(x)
+
+
+var y = array1.join(" / ")
+console.log(array1)
+console.log(x)
+console.log(y)
+
+
+var z = array1.unshift("IOS")
+console.log(array1)
+console.log(z)
+
+console.clear()
+// Exercicio 3
+var array2 = [
+    ["HTML", 1993, "CSS", 1996],
+    ["Bootstrap", 2011, "JS", 1995],
+    ["React", 2013, "Java", 1995],
+]
+console.log(array2);
+
+console.log(array2.length)
+
+console.log(array2[2][1]);
+
+array2[1][2] = "JavaScript"
+console.log(array2)
+
+delete array2[2][2]
+console.log(array2)
+
+var u = array2.push('pipoca');
+console.log(array2)
+console.log(u)
